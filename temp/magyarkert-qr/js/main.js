20d93231a93def144ab90276697d1ee856ce651a
István Lantos <kerozin.joe@gmail.com>
2017-10-13 17:45:29 +0200

    2017. 10. 13. 17:45:29,09

--- temp/magyarkert-qr/js/main.js
+++ temp/magyarkert-qr/js/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+/*
+    MODEL LIST:
+    https://mesziair.hu/temp/magyarkert-ar/?model=parlament&debug=true
+    https://mesziair.hu/temp/magyarkert-ar/?model=sandor-palota&debug=true
+*/
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    var results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var model = getParameterByName('c');
+
+var data = {
+    city: c,
+};
+
+console.log(`city == ${data.city}`);
