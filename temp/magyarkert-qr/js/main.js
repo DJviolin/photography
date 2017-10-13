d4e88d4b1601e75aaa5e807c6b3d37b5919e7dc5
István Lantos <kerozin.joe@gmail.com>
2017-10-13 17:57:02 +0200

    2017. 10. 13. 17:57:01,82

--- temp/magyarkert-qr/js/main.js
+++ temp/magyarkert-qr/js/main.js
@@ -2,7 +2,7 @@
 
 /*
     URL LIST:
-    http://magyarkert.com/qr/?c=jakabszallas
+    http://magyarkert.com/qr/?city=jakabszallas
 */
 
 function getParameterByName(name, url) {
@@ -15,10 +15,34 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-var model = getParameterByName('c');
+var city = getParameterByName('city');
 
 var data = {
-    city: c,
+    city: city,
 };
 
 console.log(`city == ${data.city}`);
+
+
+
+function getJSON(city) {
+    var link = 'http://magyarkert.com/qr/data/' + city + '/data.json';
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+      } else {
+        // We reached our target server, but it returned an error
+    
+      }
+    };
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+    request.send();
+}
+
+console.log(`json == ${getJSON('jakabszallas')}`);
