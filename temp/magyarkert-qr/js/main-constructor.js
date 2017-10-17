c08be86c21236343a3d2b05ac99a21d0043333b9
István Lantos <kerozin.joe@gmail.com>
2017-10-17 14:24:26 +0200

    2017. 10. 17. 14:24:26,29

--- temp/magyarkert-qr/js/main-constructor.js
+++ temp/magyarkert-qr/js/main-constructor.js
@@ -18,6 +18,29 @@
 	https://github.com/newbreedofgeek/dynamic-js-script-loader
 */
 
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+	var method;
+	var noop = function () {};
+	var methods = [
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+	];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
+
+	while (length--) {
+		method = methods[length];
+
+		// Only stub undefined methods.
+		if (!console[method]) {
+			console[method] = noop;
+		}
+	}
+}());
+
 var app = {
     // Application Constructor
     initialize: function () {
