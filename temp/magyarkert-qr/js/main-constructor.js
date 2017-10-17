18eb6fe678e8751a54c0f29727af8bd463276689
István Lantos <kerozin.joe@gmail.com>
2017-10-17 13:26:49 +0200

    2017. 10. 17. 13:26:49,19

--- temp/magyarkert-qr/js/main-constructor.js
+++ temp/magyarkert-qr/js/main-constructor.js
@@ -25,9 +25,6 @@ var app = {
     },
 
     // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
